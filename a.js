6c4154136a00abcafa6b26322222ecc5748e5841
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-12 20:18:19 +0700

    server

--- a.js
+++ a.js
@@ -1,11 +1,13 @@
 const request = require('request');
 
-function getTemp(cityName) {
+function getTemp(cityName, cb) {
     const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=' + cityName;
     request(URL, (err, res, body) => {
         const obj = JSON.parse(body);
-        console.log(obj.main.temp);
+        // temp = obj.main.temp;
+        cb(obj.main.temp);
     });
 }
 
-getTemp('New York');
+// console.log(getTemp('New York'));
+getTemp('New York', t => console.log(t));
