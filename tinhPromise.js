1812193d1632da55d164529e3c594741947e76bd
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-14 20:04:49 +0700

    a

--- tinhPromise.js
+++ tinhPromise.js
@@ -10,6 +10,27 @@ function congPromise(a, b) {
     });
 }
 
+function nhanPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error'));
+        request(`http://localhost:3000/tinh/NHAN/${a}/${b}`, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
+
+function chiaPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        if (b == 0) return reject(new Error('Divide by zero'));
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error'));
+        request(`http://localhost:3000/tinh/CHIA/${a}/${b}`, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
+
 function getTempPromise(cityName) {
     return new Promise((resolve, reject) => {
         const preURL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
@@ -25,9 +46,9 @@ function getTempPromise(cityName) {
     });
 }
 
-getTempPromise('xcdcd')
-.then(temp => console.log(`Temp: ` + temp))
-.catch(err => console.log(err.message));
+// getTempPromise('xcdcd')
+// .then(temp => console.log(`Temp: ` + temp))
+// .catch(err => console.log(err.message));
 
 // congPromise(4, 5)
 // .then((result) => console.log(result))
