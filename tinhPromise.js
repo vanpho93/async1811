3c607d5407a9bb3d0b9f0c1462271a1f15e5dacf
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-14 20:01:30 +0700

    reject

--- tinhPromise.js
+++ tinhPromise.js
@@ -10,6 +10,25 @@ function congPromise(a, b) {
     });
 }
 
-congPromise(4, 5)
-.then((result) => console.log(result))
-.catch((err) => console.log(err))
+function getTempPromise(cityName) {
+    return new Promise((resolve, reject) => {
+        const preURL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
+        request(preURL + cityName, (err, res, body) => {
+            if (err) return reject(err);
+            const obj = JSON.parse(body);
+            // temp = obj.main.temp;
+            if (!obj.main) {
+                return reject(new Error(obj.message));
+            }
+            resolve(obj.main.temp);
+        });
+    });
+}
+
+getTempPromise('xcdcd')
+.then(temp => console.log(`Temp: ` + temp))
+.catch(err => console.log(err.message));
+
+// congPromise(4, 5)
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
